67d3e125d392153537ba55eaa504d8a9717c5615
Vikram <vikramanand858@gmail.com>
2026-03-31 20:16:17 +0530

    fixed settings path

--- Backend/edututor/edututor/wsgi.py
+++ Backend/edututor/edututor/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edututor.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edututor.edututor.settings')
 
 application = get_wsgi_application()
